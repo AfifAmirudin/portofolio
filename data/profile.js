bac2751bc961cfd1eb062c83a68fc2eeaba5df60
Afif Amirudin <72907008+AfifAmirudin@users.noreply.github.com>
2026-01-28 21:13:23 +0700

    Update profile.js
    edit role, bio, and cv link

--- data/profile.js
+++ data/profile.js
@@ -1,12 +1,12 @@
 export const profile = {
   name: "Afif Amirudin",
-  role: "Graphic & Video Designer",
-  bio: "I craft visual stories through design, motion, and digital media.",
+  role: "Creative Digital Marketing",
+  bio: "I craft visual stories through design, motion, and digital media. A results-driven, creative digital marketing specialist with over 2+ years of experience designing compelling campaigns to drive engagement and conversions. Expert in social media strategy, content creation, and data-driven marketing.",
 
   contact: {
     email: "mailto:afifamirudin44@gmail.com",
     whatsapp: "https://wa.me/6282177766998",
-    cv: "https://drive.google.com/file/d/1OmSdEJfRXRWJFZboidR7YkSqGZyhTPnB/view?usp=sharing"
+    cv: "https://cv-aafif.vercel.app"
   },
 
 
